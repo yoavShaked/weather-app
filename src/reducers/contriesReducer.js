32e2a3ef716a0f0c4b58a3e768d7fa99e98ae236
Yoav Shaked <yoavcad@gmail.com>
2020-04-14 20:29:22 +0300

    Fix typo

--- src/reducers/contriesReducer.js
+++
@@ -1,9 +0,0 @@
-import * as types from '../actions/types';
-
-const initialState = {};
-
-const contriesReducer = (state = initialState, action) => {
-return state;
-};
-
-export default contriesReducer;
