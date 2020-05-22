0ae77c9bfb3c4f157cd39d25bc95d59105bb9f35
Yoav Shaked <yoavcad@gmail.com>
2020-05-22 14:05:08 +0300

    import style to the root

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./store";
+import './style.css';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
