5dd4f8017657f5d59468b8f7ef7e4466e4fcfd69
Yoav Shaked <yoavcad@gmail.com>
2020-05-22 14:02:32 +0300

    Nevigationbar created

---
+++ src/components/common/Nevigationbar.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+import styled from "styled-components";
+import _ from 'lodash';
+
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+
+import Flexbox from "./Flexbox";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const pathCode = {
+  '': 0,
+  "favorites": 1
+};
+
+export default function Nevigationbar() {
+  const classes = useStyles();
+  const windowLocationPathName = _.trim(window.location.pathname, '/');
+  const pathCodeValue = _.get(pathCode, windowLocationPathName);
+
+  const [value, setValue] = useState(pathCodeValue);
+  
+  const handleChange = (_, newValue) => setValue(newValue);
+
+  return (
+    <Container className={classes.root}>
+      <Typography>Herolo Weather</Typography>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+      >
+        <Tab label={<Link to="/">Home</Link>} />
+        <Tab label={<Link to="/favorites">Favorites</Link>} />
+      </Tabs>
+    </Container>
+  );
+}
+
+const Container = styled(Flexbox)`
+  justify-content: space-between;
+  background-color: blue;
+
+  .MuiTabs-root {
+    height: 80px;
+  }
+  .MuiTabs-flexContainer {
+    height: 100%;
+  }
+  .MuiTypography-body1 {
+    width: fit-content;
+    height: fit-content;
+    margin: auto 0 auto 30px;
+  }
+`;
