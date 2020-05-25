22de2733847a10c8ec3f969eb9c9f91b5fbe694a
Yoav Shaked <yoavcad@gmail.com>
2020-05-25 13:13:01 +0300

    simplified log middleware

--- src/middlewares/loggerMiddleware.js
+++ src/middlewares/loggerMiddleware.js
@@ -1,7 +1,6 @@
-const logger = store => next => action => {
-  console.log('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  return result
-}
+const logger = (store) => (next) => (action) => {
+  console.log("dispatching", action);
+  console.log("prev state", store.getState());
+  return next(action);;
+};
 export default logger;
