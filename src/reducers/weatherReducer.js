c50a6906ae59fc956bdf20b276b4c4bc1632894c
Yoav Shaked <yoavcad@gmail.com>
2020-05-25 13:12:23 +0300

    weather reducer created - WIP

---
+++ src/reducers/weatherReducer.js
@@ -0,0 +1,40 @@
+import { set, flow, get } from "lodash/fp";
+
+import * as types from "../actions/types";
+import { mock_locationAutocomplete } from "../apiRequests";
+
+const initialState = {
+  isLoading: false,
+  suggestedLocations: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case types.GET_LOCATION_AUTOCOMPLETE.START: {
+      const newState = set("isLoading", true, state);
+      console.log("next state", newState);
+      return newState;
+    }
+    case types.GET_LOCATION_AUTOCOMPLETE.RESOLVED: {
+      const newState = flow([
+        set("isLoading", false),
+        set("suggestedLocations", action.payload),
+      ])(state);
+      console.log("next state", newState);
+      return newState;
+    }
+    case types.GET_LOCATION_AUTOCOMPLETE.ERROR: {
+      return Object.assign(
+        state,
+        {},
+        {
+          isLoading: false,
+          suggestedLocations: [...mock_locationAutocomplete],
+          error: true,
+        }
+      );
+    }
+    default:
+      return state;
+  }
+};
