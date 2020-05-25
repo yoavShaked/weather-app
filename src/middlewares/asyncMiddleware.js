66e0a663ce94e04c5080d12b925c77036463ed97
Yoav Shaked <yoavcad@gmail.com>
2020-05-25 13:09:49 +0300

    Refactoring async middleware - calling fetch from middleware

--- src/middlewares/asyncMiddleware.js
+++ src/middlewares/asyncMiddleware.js
@@ -1,38 +1,29 @@
-import * as types from "../actions/types";
+import { get } from "lodash/fp";
 
-function handleResponse(response, next, action) {
-  const { error } = response;
-  if (error) {
-    next({
-      type: action.type.ERROR,
-    });
-  } else {
-    next({
-      type: action.type.RESOLVED,
-      payload: response.data,
-    });
-  }
-}
-const asyncMiddleware = (_) => (next) => (action) => {
-  const httpAction = action["HTTP_ACTION"];
-
-  if (httpAction) {
-    const { request } = httpAction;
-
-    next({
-      type: httpAction.type.START,
-    });
-
-    request()
-      .then((data) => {
-        handleResponse(data, next, httpAction);
-      })
-      .catch((_) => {
-        handleResponse({ error: true }, next, httpAction);
-      });
-  } else {
+const asyncMiddleware = (store) => (next) => (action) => {
+  const httpAction = get("HTTP_ACTION", action);
+  
+  if (!httpAction) {
     return next(action);
   }
+
+  const { endpoint } = httpAction;
+
+  next({ type: httpAction.type.START});
+
+  fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      store.dispatch({
+        type: httpAction.type.RESOLVED,
+        payload: data,
+      });
+    })
+    .catch((err) => {
+      store.dispatch({
+        type: httpAction.type.ERROR,
+      });
+    });
 };
 
 export default asyncMiddleware;
