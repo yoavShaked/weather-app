8a1bbd2e56c287579a3f1afeb66ee29d57361083
Yoav Shaked <yoavcad@gmail.com>
2020-05-25 13:49:31 +0300

    remove extra smicolon

--- src/middlewares/loggerMiddleware.js
+++ src/middlewares/loggerMiddleware.js
@@ -1,6 +1,6 @@
 const logger = (store) => (next) => (action) => {
   console.log("dispatching", action);
   console.log("prev state", store.getState());
-  return next(action);;
+  return next(action);
 };
 export default logger;
