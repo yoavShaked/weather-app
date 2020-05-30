b386bbbb831016c48b4331fff2e8f3fdfb860ff9
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 18:27:26 +0300

    using flow

--- src/utils/degrees.js
+++ src/utils/degrees.js
@@ -1,17 +1,20 @@
-import {UNIT_TYPE} from '../constants/titles';
+import { flow } from "lodash/fp";
+import { UNIT_TYPE } from "../constants/titles";
 
 const convertToF = (value) => (9 / 5) * value + 32;
 
-const convertToC = (value) => (value - 32) * 5/ 9;
+const convertToC = (value) => ((value - 32) * 5) / 9;
 
-const roundToOneDecimalNumberAfterPoint = (number) => Math.round(number * 10) / 10;
+const roundToOneDecimalNumberAfterPoint = (number) =>
+  Math.round(number * 10) / 10;
 
 export const convertDegreeValue = (dest, value) => {
   switch (dest) {
     case UNIT_TYPE.FAHRENHEIT:
-      return roundToOneDecimalNumberAfterPoint(convertToF(value));
+      return flow([convertToF, roundToOneDecimalNumberAfterPoint])(value);
     case UNIT_TYPE.CELSIUS:
-      return roundToOneDecimalNumberAfterPoint(convertToC(value));
-    default: return value;
+      return flow([convertToC, roundToOneDecimalNumberAfterPoint])(value);
+    default:
+      return value;
   }
 };
