f0e7bec8a34b217ea5969c9214ca4b79b50a7c5e
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 16:42:39 +0300

    using in roundToOneDecimalNumberAfterPoint

--- src/utils/degrees.js
+++ src/utils/degrees.js
@@ -4,12 +4,14 @@ const convertToF = (value) => (9 / 5) * value + 32;
 
 const convertToC = (value) => (value - 32) * 5/ 9;
 
+const roundToOneDecimalNumberAfterPoint = (number) => Math.round(number * 10) / 10;
+
 export const convertDegreeValue = (dest, value) => {
   switch (dest) {
     case UNIT_TYPE.FAHRENHEIT:
-      return convertToF(value);
+      return roundToOneDecimalNumberAfterPoint(convertToF(value));
     case UNIT_TYPE.CELSIUS:
-      return convertToC(value);
+      return roundToOneDecimalNumberAfterPoint(convertToC(value));
     default: return value;
   }
 };
