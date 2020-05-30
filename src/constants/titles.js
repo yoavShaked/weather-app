4c9b7ebce60ba2d8351bc6ef333fea2636dfa8a4
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 16:57:09 +0300

    adding enums DAY_TIME, UNIT_TYPE

--- src/constants/titles.js
+++ src/constants/titles.js
@@ -4,3 +4,13 @@ export const TABS = {
 };
 
 export const APP_TITLE = "Herolo Weather";
+
+export const DAY_TIME = {
+    DAY: 'Day',
+    NIGHT: 'Night'
+};
+
+export const UNIT_TYPE = {
+    FAHRENHEIT: 'Fahrenheit',
+    CELSIUS: 'Celsius'
+};
