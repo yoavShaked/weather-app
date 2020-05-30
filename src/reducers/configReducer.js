f7ad14b44e35b746d9f20188927bee5f993192e7
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 15:25:39 +0300

    remove day time form reducer state

--- src/reducers/configReducer.js
+++ src/reducers/configReducer.js
@@ -3,17 +3,13 @@ import {UNIT_TYPE, DAY_TIME} from '../constants/titles';
 import * as types from "../actions/types";
 
 const initialState = {
-  unitType: UNIT_TYPE.CELSIUS,
-  dayTime: DAY_TIME.DAY,
+  unitType: UNIT_TYPE.CELSIUS
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.SET_DEGREE_UNIT_TYPE: {
       return set("unitType", get("unitType", action.payload), state);
-    }
-    case types.SET_DAY_TIME: {
-      return set("dayTime", get("dayTime", action.payload), state);
     }
     default: {
       return state;
