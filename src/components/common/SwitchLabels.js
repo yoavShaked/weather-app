94e4adf8f2abe7df45785d46bdd3aff5bf231d1b
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 14:28:36 +0300

    SwitchLabels created

---
+++ src/components/common/SwitchLabels.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { isFunction } from "lodash/fp";
+import { FormControlLabel, Switch } from "@material-ui/core";
+
+export default function SwitchLabels({
+  checkedLabel,
+  uncheckedLabel,
+  afterChange,
+}) {
+  const [state, setState] = useState({
+    checked: true,
+    label: checkedLabel,
+  });
+
+  const handleChange = (event) => {
+    const { checked } = event.target;
+    const label = checked ? checkedLabel : uncheckedLabel;
+    if (isFunction(afterChange)) {
+      afterChange(label);
+    }
+    setState({
+      checked,
+      label,
+    });
+  };
+
+  return (
+      <FormControlLabel
+        control={
+          <Switch
+            checked={state.checked}
+            onChange={handleChange}
+            name="checked"
+            color="primary"
+          />
+        }
+        label={state.label}
+      />
+  );
+}
+
+SwitchLabels.propTypes = {
+  checkedLabel: PropTypes.string,
+  uncheckedLabel: PropTypes.string,
+  afterChange: PropTypes.func,
+};
