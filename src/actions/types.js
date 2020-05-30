3461a5a69db24195c3ef10dd95225dd9b8adbd32
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 11:56:12 +0300

    adding meta to action creator and adding action creator

--- src/actions/types.js
+++ src/actions/types.js
@@ -8,3 +8,4 @@ export const GET_DAILY_FORCAST = createAsyncActionType("GET_DAILY_FORCAST");
 export const GET_CURRENT_WEATHER = createAsyncActionType("GET_CURRENT_WEATHER");
 export const GET_LOCATION_AUTOCOMPLETE = createAsyncActionType("GET_LOCATION_AUTOCOMPLETE");
 export const SET_INITIAL_FORCAST = createAsyncActionType("SET_INITIAL_FORCAST");
+export const SET_DEGREE_UNIT_TYPE = "SET_DEGREE_UNIT_TYPE";
--- src/actions/weather.js
+++ src/actions/weather.js
@@ -2,26 +2,38 @@ import * as types from "./types";
 import httpAction from "./httpRquest";
 import * as weatherRequestsApi from "../apiRequests";
 
-export const getDailyForcast = (locationId) =>
+export const getDailyForcast = (locationId, meta = {}) =>
   httpAction({
     type: types.GET_DAILY_FORCAST,
     endpoint: weatherRequestsApi.getDailyForcast(locationId),
+    meta,
   });
 
-export const getLocationCurrentWeather = (locationId) =>
+export const getLocationCurrentWeather = (locationId, meta = {}) =>
   httpAction({
     type: types.GET_CURRENT_WEATHER,
     endpoint: weatherRequestsApi.getLocationCurrentWeather(locationId),
+    meta,
   });
 
-export const getLocationAutocomplete = (locationQuery) =>
+export const getLocationAutocomplete = (locationQuery, meta = {}) =>
   httpAction({
     type: types.GET_LOCATION_AUTOCOMPLETE,
     endpoint: weatherRequestsApi.getLocationAutocomplete(locationQuery),
+    meta,
   });
 
-export const setInitialForcast = () =>
+export const setInitialForcast = (meta = {}) =>
   httpAction({
     type: types.SET_INITIAL_FORCAST,
     endpoint: weatherRequestsApi.getLocationAutocomplete("Tel Aviv"),
+    meta,
+  });
+
+  export const setUnitType = (source, dest) => ({
+    type: types.SET_DEGREE_UNIT_TYPE,
+    payload: {
+      sourceUnitType: source,
+      destUnitType: dest
+    }
   });
