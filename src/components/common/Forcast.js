5a7286fc2e2b16106f7fea2dd27cef54a3d5d0d2
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 15:18:04 +0300

    Refactoring forcast

--- src/components/common/Forcast.js
+++ src/components/common/Forcast.js
@@ -1,22 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { get } from "lodash/fp";
+import { Typography } from "@material-ui/core";
 
 import * as weatherActions from "../../actions/weather";
+import { DAY_TIME } from "../../constants/titles";
 
-const Forcast = ({ setInitialForcast, dailyForcast }) => {
+import Flexbox from "./Flexbox";
+import SwitchLabels from "./SwitchLabels";
+import WeeklyForcast from "./WeeklyForcast";
+
+const Forcast = ({ setInitialForcast, cityName, description, isLoading }) => {
+  const [dayTime, setDayTime] = useState(DAY_TIME.DAY);
   useEffect(() => {
-    setInitialForcast();
+    setInitialForcast({ cityName: "Tel Aviv" });
   }, []);
-  
-  if (!dailyForcast) return <div>Loading</div>;
-  return <div>Forcast</div>;
+
+  if (isLoading) return <div>Loading</div>;
+  return (
+    <Flexbox flexDirection="column">
+      <Flexbox>
+        <Typography>{cityName}</Typography>
+        <SwitchLabels
+          checkedLabel={DAY_TIME.DAY}
+          uncheckedLabel={DAY_TIME.NIGHT}
+          afterChange={setDayTime}
+        />
+      </Flexbox>
+      <Typography>{description}</Typography>
+      <WeeklyForcast dayTime={dayTime} />
+    </Flexbox>
+  );
 };
 
 const mapStateToProps = (state) => ({
-  dailyForcast: get(["forcast", "dailyForcast"], state),
+  cityName: get(["forcast", "cityName"], state),
+  isLoading: get(["forcast", "isLoading"], state),
+  description: get(["forcast", "description"], state),
 });
 
 export default connect(mapStateToProps, {
