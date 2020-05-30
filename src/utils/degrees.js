42bfc2302b4db6d2e4b57dc43c7081601eaf4335
Yoav Shaked <yoavcad@gmail.com>
2020-05-30 14:32:04 +0300

    supporting only c, f degree unit types after checking with api response

--- src/utils/degrees.js
+++ src/utils/degrees.js
@@ -1,44 +1,13 @@
-const convertToF = (source, value) => {
-  switch (source) {
-    case "C":
-      return (9 / 5) * value + 32;
-    case "K":
-      return (9 / 5) * value - 459.67;
-    case "R":
-      return value - 459.67;
-    default:
-      return value;
-  }
-};
+const convertToF = (value) => (9 / 5) * value + 32;
 
-const convertToC = (source, value) => {
-  switch (source) {
-    case "F":
-      return ((value - 32) * 5) / 9;
-    case "K":
-      return value - 273.15;
-    case "R":
-      return ((value - 491.67) * 5) / 9;
-    default:
-      return value;
-  }
-};
+const convertToC = (value) => (value - 32) * 5/ 9;
 
-export const convertDegreeValue = (source, dest, value) => {
+export const convertDegreeValue = (dest, value) => {
   switch (dest) {
     case "F":
-      return convertToF(source, value);
+      return convertToF(value);
     case "C":
-      return convertToC(source, value);
+      return convertToC(value);
     default: return value;
-  }
-};
-
-export const getSourceDestUnitType = (changedUnitType, source, dest) => {
-  switch (changedUnitType) {
-    case source:
-      return { source: dest, dest: source };
-    case dest:
-      return { source, dest };
   }
 };
