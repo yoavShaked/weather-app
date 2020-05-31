1cedd84c685fb4792a5a3bc7a5a889643e763c72
Yoav Shaked <yoavcad@gmail.com>
2020-05-31 09:51:42 +0300

    removes

--- src/actions/countries.js
+++
@@ -1,8 +0,0 @@
-import * as types from './types';
-import httpAction from './httpRquest';
-import httpRequest from '../endpoints/index';
-
-export const getAllCountries = () => httpAction({
-    type: types.GET_CUNTRIES,
-    request: httpRequest
-});
