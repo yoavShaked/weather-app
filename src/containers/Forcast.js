58dc203d6c7d39c2705f3c89b8b967c348b74d2b
Yoav Shaked <yoavcad@gmail.com>
2020-05-31 18:06:52 +0300

    style forcast - WIP

--- src/containers/Forcast.js
+++ src/containers/Forcast.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
+import moment from 'moment';
 import { get } from "lodash/fp";
 import { Typography } from "@material-ui/core";
-
+import styled from 'styled-components';
 import * as weatherActions from "../actions/weather";
 import { DAY_TIME, UNIT_TYPE } from "../constants/titles";
 
@@ -29,22 +30,50 @@ const Forcast = ({
   }, []);
 
   if (isLoading) return <div>Loading</div>;
+
+  const currentFullTime = `${moment().format('dddd')} ${moment().format('MMMM Do YYYY, h:mm:ss a')}`;
+
   return (
-    <Flexbox flexDirection="column">
-      <Flexbox>
-        <Typography>{cityName}</Typography>
+    <Container flexDirection="column">
+      <Typography>{cityName}</Typography>
+      <Typography>{currentFullTime}</Typography>
+      <Typography>{description}</Typography>
+      <ActionsContainer>
         <SwitchLabels
           checkedLabel={DAY_TIME.DAY}
           uncheckedLabel={DAY_TIME.NIGHT}
           afterChange={setDayTime}
         />
-        <FavoriteAction/>
-      </Flexbox>
-      <Typography>{description}</Typography>
+        <FavoriteAction />
+      </ActionsContainer>
       <WeeklyForcast dayTime={dayTime} />
-    </Flexbox>
+    </Container>
   );
 };
+
+const Container = styled(Flexbox)`
+  width: 800px;
+  margin: 40px auto auto auto;
+  .MuiTypography-body1 {
+    margin: 0 auto;
+  }
+`;
+
+const ActionsContainer = styled(Flexbox)`
+  justify-content: space-around;
+  margin: 30px 0;
+`;
+
+const Top = styled(Flexbox)`
+    width: inherit;
+    justify-content: space-between;
+`;
+
+const TopRight = styled(Flexbox)`
+  width: 210px;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 const mapStateToProps = (state) => ({
   cityName: get(["forcast", "cityName"], state),
