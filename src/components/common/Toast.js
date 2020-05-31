c8b76c358c2338620a7eae5e347382e0b8e0151f
Yoav Shaked <yoavcad@gmail.com>
2020-06-01 01:28:24 +0300

    Toast created

---
+++ src/components/common/Toast.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes  from 'prop-types';
+
+import { Typography } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
+
+const Toast = ({severity, context}) => {
+    return (
+      <Alert severity={severity}>
+        <Typography>{context}</Typography>
+      </Alert>
+    );
+};
+
+Toast.propTypes = {
+  severity: PropTypes.oneOf(["success", "error"]),
+  context: PropTypes.string.isRequired
+};
+
+Toast.defaultProps = {
+  severity: 'error'
+};
+
+export default Toast;
