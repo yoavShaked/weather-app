e330f073c51ada155bb40f7ff5dcc5efebbe0f81
Yoav Shaked <yoavcad@gmail.com>
2020-05-31 18:06:26 +0300

    style nevgition bar

--- src/components/common/Nevigationbar.js
+++ src/components/common/Nevigationbar.js
@@ -9,7 +9,7 @@ import { Tabs, Tab, Typography } from "@material-ui/core";
 import { TABS, APP_TITLE } from "../../constants/titles";
 
 import Flexbox from "./Flexbox";
-import Config from './Config';
+import Config from "./Config";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,8 +35,8 @@ export default function Nevigationbar() {
   return (
     <Container className={classes.root}>
       <Flexbox>
-        <Typography>{APP_TITLE}</Typography>
-        <Config/>
+        <Typography className='title'>{APP_TITLE}</Typography>
+        <Config className="Config" />
       </Flexbox>
       <Tabs value={value} onChange={handleChange}>
         <Tab label={<Link to="/">{TABS.HOME}</Link>} />
@@ -48,17 +48,37 @@ export default function Nevigationbar() {
 
 const Container = styled(Flexbox)`
   justify-content: space-between;
-  background-color: blue;
+  background: linear-gradient(
+    90deg,
+    rgba(84, 84, 171, 1) 0%,
+    rgba(11, 219, 244, 1) 100%,
+    rgba(3, 0, 46, 1) 100%
+  );
 
   .MuiTabs-root {
     height: 80px;
   }
   .MuiTabs-flexContainer {
     height: 100%;
+    a {
+      color: #fff;
+      text-decoration: none;
+      text-transform: capitalize;
+    }
   }
-  .MuiTypography-body1 {
+  .title {
     width: fit-content;
     height: fit-content;
+    color: #fff;
     margin: auto 0 auto 30px;
+  }
+  .MuiTypography-body1 {
+    color: #fff;
+  }
+  .Config {
+    margin: auto 0 auto 10px;
+  }
+  .MuiFormControlLabel-root {
+    margin-left: 50px;
   }
 `;
