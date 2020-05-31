fe318d7b0abb9a1c2a3fe968221c3c7737f53aa6
Yoav Shaked <yoavcad@gmail.com>
2020-05-31 18:59:44 +0300

    adding shadow

--- src/containers/WeeklyForcast.js
+++ src/containers/WeeklyForcast.js
@@ -23,12 +23,20 @@ const WeeklyForcast = ({ weatherForcast, dayTime }) => {
     );
   };
 
-  return <Flexbox>{weatherForcast.map(mapWeather)}</Flexbox>;
+  return <Container>{weatherForcast.map(mapWeather)}</Container>;
 };
 
 const WeatherItemContainer = styled(Flexbox)`
   margin: 20px;
   flex-wrap: wrap;
+`;
+const Container = styled(Flexbox)`
+    border: 1px solid;
+    border-radius: 7px;
+    height: 209px;
+    border-color: #27c2ffc7;
+    -webkit-box-shadow: 2px 2px 13px 5px #27c2ffc7;
+    box-shadow: 2px 2px 13px 5px rgba(39,194,255,0.78);
 `;
 
 const mapStateToProps = (state) => ({
