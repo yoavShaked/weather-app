b0e7bc8852d59b0d2b8eaff6c804d9939f18ee24
Yoav Shaked <yoavcad@gmail.com>
2020-06-02 15:25:03 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { Provider } from "react-redux";
-
-import configureStore from "./store";
 
 import Main from './Main';
 
 const App = () => {
    return (
-     <Provider store={configureStore()}>
        <Main />
-     </Provider>
    );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+
+import configureStore from "./store";
 
 import './style.css';
 
 ReactDOM.render(
-    <App />,
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
   document.getElementById("index")
 );
